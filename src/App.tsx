6b0db103bbc7aeaaec9afdddbd5128e9b48d1b6d
Asyraf Omar <asyrafomar68@gmail.com>
2024-07-17 12:43:04 +0800

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,20 @@
 import React from 'react';
 import logo from './assets/logo.svg';
 import ExampleComp from './components/exampleComponent/ExampleComp'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/Login/Login';
+
 
 function App() {
-  const handleClick = () => {
-    alert('Button clicked!');
-  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"  
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <ExampleComp title="Welcome!" isActive={true} onClick={handleClick} />
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+        </Routes>
+    </Router>
   );
 }
 
