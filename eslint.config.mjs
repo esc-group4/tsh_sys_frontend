e9f4d8f7481e69c12641a5d43caefb7ec46b054d
Asyraf Omar <asyrafomar68@gmail.com>
2024-07-18 15:23:42 +0800

    modify jest configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,11 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import { ESLint } from 'eslint';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-);
+export default [
+  { parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier'
+    ] }
+];
